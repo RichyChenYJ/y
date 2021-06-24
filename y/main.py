952f916f58008c4a8252b2c63c4be3706c65b152
RichyChenYJ <travelrichyc@gmail.com>
2021-06-24 16:59:50 +0800

    Refactor code to pipeline design pattern

---
+++ y/main.py
@@ -0,0 +1,25 @@
+from y.pipeline.steps.get_video_list import GetVideoList
+from y.pipeline.steps.step import StepException
+
+from y.pipeline.pipeline import Pipeline
+
+CHANNEL_ID = 'UCKSVUHI9rbbkXhvAXK-2uxA'
+
+
+def main():
+    inputs = {
+        'channel_id': CHANNEL_ID
+    }
+    steps = [
+        GetVideoList(),
+        ]
+
+    p = Pipeline(steps)
+    p.run(inputs)
+
+
+if __name__ == '__main__':
+    main()
+
+# video_list = get_all_video_in_channel(CHANNEL_ID)
+# print(len(video_list))
